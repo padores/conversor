e3c648f46ae06e439a19a9d11d327e2040bdcec9
Pedrosa <pedrosa@ubuntu.ubuntu-domain>
2013-09-18 20:06:16 -0300

    Criada a classe Conversor

---
+++ Conversor.java
@@ -0,0 +1,17 @@
+package test;
+public class Conversor {
+
+	public int celsiusParaFahrenheit() {
+		return (9 * 40) / 5 + 32;
+	}
+
+	public int celsiusParaFahrenheit(int valorCelcius) {
+		return (9 * valorCelcius) / 5 + 32;
+
+	}
+
+	public double celsiusParaFahrenheit(double valorCelcius) {
+		return (9 * valorCelcius) / 5 + 32;
+	}
+
+}
