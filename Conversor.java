350ccc28f70d1033940cac931f9343bd15567f20
Gabriel Pedrosa <pedrosa@outlook.com>
2013-10-04 06:39:03 -0300

    push da classe completa

--- Conversor.java
+++ Conversor.java
@@ -13,5 +13,50 @@ public class Conversor {
 	public double celsiusParaFahrenheit(double valorCelcius) {
 		return (9 * valorCelcius) / 5 + 32;
 	}
+	
+
+    int fahrenheitParaCelsius(int fahrenheit){	
+        return (fahrenheit - 32) * 5 / 9;
+    }
+
+    double fahrenheitParaCelsius(double fahrenheit){	
+        return (fahrenheit - 32.0) * 5 / 9.0;
+    }
+
+    int kelvinParaCelsius(int kelvin){	
+        return kelvin - 273;
+    }
+
+    double kelvinParaCelsius(double kelvin){	
+        return kelvin - 273.0;
+    }
+
+    int celsiusParaKelvin(int celsius){	
+        return celsius + 273;
+    }
+
+    double celsiusParaKelvin(double celsius){	
+        return celsius + 273.0;
+    }
+
+    int kelvinParaFahrenheit(int kelvin){
+        int celsius = kelvin - 273;	
+        return celsiusParaFahrenheit(celsius);
+    }
+
+    double kelvinParaFahrenheit(double kelvin){
+        double celsius = kelvin - 273.0;	
+        return celsiusParaFahrenheit(celsius);
+    }
+
+    int fahrenheitParaKelvin(int fahrenheit){
+        int celsius = fahrenheitParaCelsius(fahrenheit);	
+        return celsiusParaKelvin(celsius);
+    }
+
+    double fahrenheitParaKelvin(double fahrenheit){
+        double celsius = fahrenheitParaCelsius(fahrenheit);	
+        return celsiusParaKelvin(celsius);
+    }
 
 }
